c15c4519089fea0551eb54aa87ef1e611e7b5bfb
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-06 21:37:53 +0300

    exercise_8

--- Vue3Exercises/src/App.vue
+++ Vue3Exercises/src/App.vue
@@ -2,8 +2,7 @@
   export default {
     data() {
       return {
-       arr1: ["x", "y", "z"],
-       arr2: [1, 2, 3],
+       obj: {x: 1, y: 2, z: 3}
       }
     }
   }
@@ -11,13 +10,8 @@
 
 <template>
   <div class="body">
-    <div class="main"> 
-      <p>{{ arr1[0] }}</p>
-      <p>{{ arr1[1] }}</p>
-      <p>{{ arr1[2] }}</p>
-    </div>
     <div class="main">
-      <p>{{ arr2[0] + arr2[1] + arr2[2] }}</p> 
+      <p>{{ obj["x"] + obj["y"] + obj["z"] }}</p> 
     </div> 
   </div>     
 </template>
