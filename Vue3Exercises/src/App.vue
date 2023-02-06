2f596193d68c2f62026776d7604d083cb64123f1
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-06 21:06:03 +0300

    exercise_5

--- Vue3Exercises/src/App.vue
+++ Vue3Exercises/src/App.vue
@@ -1,45 +1,19 @@
 <script>
   export default {
-    name: 'App',
     data() {
       return {
-        name: 'Ali',
-        surn: 'Dzelyalov',
+        text: 'page',
+        href: 'page.html',
       }
     }
   }
 </script>
 
 <template>
-  <div> {{name}} </div>
-  <div> {{surn}} </div>
+  <div class="body">
+    <div class="main"> 
+      <a :href="page">Tap here)</a>
+    </div>
+  </div>    
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
