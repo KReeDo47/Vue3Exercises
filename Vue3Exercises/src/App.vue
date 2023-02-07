d14c2b7ecb35a156bd2b483a24c56adc124fc2bd
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-07 21:18:09 +0300

    exercise_17

--- Vue3Exercises/src/App.vue
+++ Vue3Exercises/src/App.vue
@@ -2,27 +2,19 @@
   export default {
     data() {
       return {
-        cost: "85",
-        amount: "15",
+        visible: true,
       };
     },
-    methods: {
-    haha: function (i) {
-      alert("haha");
-      return false;
-    },
-  },
 }; 
 </script>
 
 <template>
   <div class="body">
     <div class="main">
-      <a href="fake.html" v-on:click.prevent>
-        <p>Tap here and nothing will happen</p></a>
+      <p v-if="visible">Ali</p>
     </div>   
     <div class="main">
-      <button v-on:click.once="haha">haha</button>
+      <p v-if="!visible">Ali</p>
     </div>  
   </div>     
 </template>
