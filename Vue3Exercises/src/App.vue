600fffdb3576fcd3fb9cd8c52293634411800bfa
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-12 15:55:50 +0300

    exercise_20

--- Vue3Exercises/src/App.vue
+++ Vue3Exercises/src/App.vue
@@ -1,27 +1,34 @@
 <script>
-	export default {
-		data() {
-      return {
+  export default{
+    data(){
+      return{
         visible: true,
-	}
-  },
-  methods: {
-	hide: function() {
-		this.visible = false;
-	},
-  mide: function() {
-		this.visible = true;
-	}
-}
-  
-	}
+        visible2: true,
+        visible3: true,
+      }
+    },
+    methods: {
+      Greg: function() {
+        this.visible = !this.visible;
+      },
+      Son: function () {
+        this.visible2 = !this.visible2;
+      },
+      Kai: function () {
+        this.visible3 = !this.visible3;
+      }
+    }
+  }
 </script>
+
 <template>
-  <button @click="hide">hide</button>
-  <button @click="visible">visible</button>
-	<p v-if="visible">text</p>
-</template>
+  <button @click="Greg">Greg</button>
+  <p v-if="visible">Greg</p>
+  <button @click="Son">Son</button>
+  <p v-if="visible2">Son</p>
+  <button @click="Kai">Kai</button>
+  <p v-if="visible3">Kai</p>
+  </template>
 
 <style>
 </style>
-
