c537aac02ac276e104b67f3c6402865a4693c24a
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-12 16:49:20 +0300

    exercise_29

--- Vue3Exercises/src/App.vue
+++ Vue3Exercises/src/App.vue
@@ -1,15 +1,35 @@
 <script>
-  export default{
-    data(){
-      return{
-        arr: ['x', 'y', 'z'],
-      }
-    }
+export default{
+data(){
+  return{
+    obj: {Greg:'100$', Ron:'200$', Kai:'300$'},
   }
+},
+methods: {
+}
+}
 </script>
+
 <template>
-  <ul v-for="(elem, key) in arr"> {{ key }} {{ elem }}</ul>  
+  <ul class="styled">
+    <li v-for="elem in obj">{{ elem }}</li>
+  </ul>
+  <br>
+  <br>
+  <ul class="styled">
+    <li v-for="(key, elem) in obj"> {{ elem }} - {{ key }}</li>
+  </ul>
+  <br>
+  <br>
+  <ul class="styled">
+    <li v-for="(key, elem, index) in obj"> {{ elem }} - {{ key }} - {{ index }}</li>
+  </ul>
+  <br>
+  <br>
+  <ul class="styled">
+    <li v-for="(key, elem, index) in obj"> {{ elem }} - {{ key }} - {{ index+1 }}</li>
+  </ul>
 </template>
 
-<style>
+<style scoped>
 </style>
