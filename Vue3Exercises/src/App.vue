f9dfaa2e990e302f586b10f3abf79decf635730f
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-12 16:10:12 +0300

    exercise_22

--- Vue3Exercises/src/App.vue
+++ Vue3Exercises/src/App.vue
@@ -2,33 +2,16 @@
   export default{
     data(){
       return{
-        visible: true,
-        visible2: true,
-        visible3: true,
-      }
-    },
-    methods: {
-      Greg: function() {
-        this.visible = !this.visible;
-      },
-      Son: function () {
-        this.visible2 = !this.visible2;
-      },
-      Kai: function () {
-        this.visible3 = !this.visible3;
+        isAdmin: true,
       }
     }
   }
 </script>
 
 <template>
-  <button @click="Greg">Greg</button>
-  <p>{{visible ? 'Greg' : '!'}}</p>
-  <button @click="Son">Son</button>
-  <p>{{visible2 ? 'Son' : '!'}}</p>
-  <button @click="Kai">Kai</button>
-  <p>{{visible3 ? 'Kai' : '!'}}</p>
-  </template>
+  <p v-if="isAdmin">+++</p>
+  <p v-else>---</p>  
+</template>
 
 <style>
 </style>
