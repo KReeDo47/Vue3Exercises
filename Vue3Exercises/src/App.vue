a9408dc4977172005b4b0e841c77cb308cb3ff8c
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-18 20:21:09 +0300

    exercise_48

--- Vue3Exercises/src/App.vue
+++ Vue3Exercises/src/App.vue
@@ -1,30 +1,43 @@
 <script>
 export default{
 data(){
-return{
-    days: [],
-}
+  return{
+    order: '',
+    visible: false,
+    visible1: false,
+    visible2: false,
+  }
+},
+methods: {
+one(){
+      this.visible = true;
+},
+  two() {
+    this.visible1 = true;
+  },
+  three() {
+    this.visible2 = true;
+  }
 }
 }
 </script>
 
 <template>
-<div>
-<input type="checkbox" v-model="days" value="Monday">
-<label>Monday</label>
+<div class="one">
+<p>На чём вы играете?</p>
+<input name="radio" type="radio" v-model="instrument" value="Гитара" @click="one">
+<label for="">?</label>
 <br>
-<input type="checkbox" v-model="days" value="Thursday">
-<label>Thursday</label>
+<input name="radio" type="radio" v-model="instrument" value="Пиано" @click="two">
+<label for="">?</label>
 <br>
-<input type="checkbox" v-model="days" value="Wednesday">
-<label>Wednesday</label>
-<ol>
-<li v-for="elem in days">{{ elem }}</li>
-</ol>
+<input name="radio" type="radio" v-model="instrument" value="Скрипка" @click="three">
+<label for="">?</label>
 </div>
+
+<p> Ваш выбор: {{ instrument }}</p>
 <br>
 </template>
 
-<style>
-  
+<style scoped>
 </style>
