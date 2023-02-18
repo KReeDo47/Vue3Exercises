df752136c1520f11ff1b3961de43bb72f9dddc37
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-18 17:57:12 +0300

    exercise_39

--- Vue3Exercises/src/App.vue
+++ Vue3Exercises/src/App.vue
@@ -1,20 +1,38 @@
 <script>
 export default{
 data(){
-return{
-  obj: {
-    done: true,
-    selected: false,
-  },
-}
+  return{
+    visible: true,
+    styles: {
+      hidden: true,
+    },
+  }
+},
+methods: {
+setDone:function(){
+  this.styles.hidden = false;
+},
+setTwo: function(){
+  this.styles.hidden = true;
+},
+  toggle: function () {
+    this.visible = !this.visible;
+  }
 }
 }
 </script>
 
 <template>
-<p: class="obj">text</p>
+<button class="button" @click="toggle">toggle</button>
+<p class="hunter" v-if="visible">Oleg</p>
+<p :class="styles">Tinkoff</p>
+<button class="button" @click="setDone">visible</button>
+<button class="button" @click="setTwo">hide</button>
 </template>
 
-<style>
-  
+<style scoped>
+p.hidden{
+  display: none;
+}
+p{color:red};
 </style>
