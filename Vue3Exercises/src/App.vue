7fa9d1fc99767eb49b9ccbcc967b89f2bd79fb9f
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-26 17:36:36 +0300

    exercise_65

--- Vue3Exercises/src/App.vue
+++ Vue3Exercises/src/App.vue
@@ -1,35 +1,29 @@
 <script>
-import User from './components/Employee.vue'
+import Ali from './components/Employee.vue'
 export default {
   data() {
     return {
-      users: [
-        {
-          id: 1,
-          name: 'Ali',
-          salary: 1000,
-          age: 18
-        },
-        {
-          id: 2,
-          name: 'Ali',
-          salary: 2000,
-          age: 15
-        },
-      ],
+      
     }
   },
   components: {
-    User
+    Ali
+  },
+  methods: {
+    Ali() {
+      alert('1, 2, 3, 4');
+    },
+    Dzelyalov(){
+      alert('1, 2, 3, 4')
+    }
   }
 }
 </script>
 
 <template>
-<User v-for="User in users" :name="User.name" :salary="User.salary" :age="User.age" :key="User.id" />
+<Ali @show="Ali" @show1="Dzelyalov" />
 </template>
 
 
 <style>
-
 </style>
