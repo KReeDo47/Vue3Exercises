a1856fb7b45b61f19823f67f50accc545a002a6a
KReeDo47 <dzelyalov.a.i.21@gmail.com>
2023-02-26 15:58:35 +0300

    exercise_58

--- Vue3Exercises/src/App.vue
+++ Vue3Exercises/src/App.vue
@@ -1,56 +1,12 @@
 <script>
-export default{
-data() {
-	return {
-		users: [
-			{
-				id: 1,
-				name: 'name1',
-				salary: 100,
-				age: 30,
-			},
-			{
-				id: 2,
-				name: 'name2',
-				salary: 200,
-				age: 40,
-			},
-			{
-				id: 3,
-				name: 'name3',
-				salary: 300,
-				age: 50,
-			},
-		],
-	}
-}
-methods: {
-edit(user) {
-    user.isEdit = true;
-  },
-  save(user) {
-    user.isEdit = false;
-  },
-}
+import Ali from './components/Employee.vue'
+export default {
+  components: { Ali }
 }
 </script>
 
 <template>
-<table class="user">
-<tr v-for="user in users" :key="user.id"> 
-<template v-if="!user.isEdit">
-    {{ user.name }} {{ user.salary }} {{ user.age }} <button class="button" @click="edit(user)">edit</button>
-</template>
-<template v-else>
-  <input v-model="user.name">
-  <input v-model="user.salary">
-  <input v-model="user.age"> 
-  <button class="button" @click="save(user)">
-      save
-  </button> 
-</template>  
-</tr>
-</table>
+<Ali />
 </template>
 
 <style>
---
+++ Vue3Exercises/src/components/Employee.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data() {
+        return {
+name: "Ali Dzelyalov"
+        }
+    }
+}
+</script>
+
+<template>
+   <p class="one">{{ name }}</p>
+</template>
